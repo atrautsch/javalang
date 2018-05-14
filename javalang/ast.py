37d5e16b28066c717d0500221a817350825c5a63
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2018-05-14 16:02:36 +0200

    add iterative as extra method

--- javalang/ast.py
+++ javalang/ast.py
@@ -48,8 +48,17 @@ class Node(object):
     def __iter__(self):
         return walk_tree(self)
 
+    def walk_tree_iterative(self):
+        return walk_tree_iterative(self)
+
     def filter(self, pattern):
         for path, node in self:
+            if ((isinstance(pattern, type) and isinstance(node, pattern)) or
+                (node == pattern)):
+                yield path, node
+
+    def filter_iterative(self, pattern):
+        for path, node in self.walk_tree_iterative():
             if ((isinstance(pattern, type) and isinstance(node, pattern)) or
                 (node == pattern)):
                 yield path, node
@@ -57,7 +66,7 @@ class Node(object):
     @property
     def children(self):
         return [getattr(self, attr_name) for attr_name in self.attrs]
-    
+
     @property
     def position(self):
         if hasattr(self, "_position"):
@@ -69,7 +78,7 @@ class Node(object):
             return self._end_position
 
 
-def walk_tree2(root):
+def walk_tree(root):
     """The standard recursive ast dfs traversal from javalang."""
     children = None
 
@@ -86,25 +95,25 @@ def walk_tree2(root):
 
 
 def _parent_path(node, parent_links):
-    """We are ineffectively iterating over a list of tuples here."""
+    """We follow the parents links path back to the root."""
     pp = ()
 
     while node:
-        for pl in parent_links:
-            if pl[0] == node:
-                if pl[1]:  # CompilationUnit has None as parent but we do not want that added
-                    pp = (pl[1],) + pp
-                node = pl[1]
-                break
+        if isinstance(node, (list, set)):
+            key = frozenset(node)
         else:
-            break
+            key = node
+        parent = parent_links[key]
+        if parent:  # CompilationUnit has None as parent but we do not want that added
+            pp = (parent,) + pp
+        node = parent
     return pp
 
 
-def walk_tree(root):
+def walk_tree_iterative(root):
     """We iteratively dfs the ast."""
     qu = deque()
-    parent_links = [(root, None)]  # parent or child can be a list so we can not use a key value pair
+    parent_links = {root: None}  # parent or child can be a list so we can not use a key value pair
 
     yield (), root
 
@@ -118,7 +127,13 @@ def walk_tree(root):
             children = node.children
 
         for c in reversed(children):  # reverse the children because we emulate a stack
-            parent_links.append((c, node))
+
+            # if we have an unhashable we frozenset it s.t. we can use it as key in dict
+            key = c
+            if isinstance(c, (list, set)):
+                key = frozenset(c)
+
+            parent_links[key] = node
             qu.append(c)
 
         try:
